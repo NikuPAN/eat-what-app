15b008ce53bbc2029f008fd5e2d85cddd948a0cf
NikuPAN <nick95927@gmail.com>
2024-07-16 17:02:35 +1000

    Added two new stars to our app. Except myself lol

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Text, Button, Image, Box } from '@chakra-ui/react'
+import { Text, Button, Image, Box, Flex } from '@chakra-ui/react'
 
 import '../App.css';
 
-const Home = () => (
-  <div className="container">
-    <Text fontSize="3xl">Welcome to Eat What App!</Text>
-    <Image
-			className='rotate-image'
-			borderRadius='full'
-			boxSize='200px'
-			src='./timmy.jpg'
-    />
-		<Box boxSize={20}></Box>
-		<Image
-			className='rotate-image-reverse'
-			borderRadius='full'
-			boxSize='200px'
-			src='./dicky.jpg'
-    />
-		<Box boxSize={50}></Box>
-		<Link to="/random">
-			<Button width="300px" colorScheme="teal" size='lg'>Help Me Choose!</Button>
-		</Link>
-		<Link to="/list">
-			<Button width="300px" colorScheme="teal" size='lg'>Restaurant List</Button>
-		</Link>
-  </div>
-);
+const Home = () => {
+	const imagesPath = [
+		['./evo.jpg', './timmy.jpg'],
+		['./dicky.jpg', './lance.jpg'],
+	];
+
+	return (
+		<div className="container">
+			<Text fontSize="3xl">Eat What?! App</Text>
+			<Box boxSize={50}></Box>
+			<Box align="center" className="rotate-image-reverse">
+			{imagesPath.map((row, rowIndex) => (
+				<Flex direction="row" key={rowIndex}>
+					{row.map((image, imageIndex) => (
+						<Image
+							key={imageIndex}
+							className="rotate-image"
+							borderRadius="full"
+							boxSize="150px"
+							src={image}
+						/>
+					))}
+				</Flex>
+			))}
+		</Box>
+			<Box boxSize={50}></Box>
+			<Link to="/random">
+				<Button width="300px" colorScheme="teal" size='lg'>Help Me Choose!</Button>
+			</Link>
+			<Link to="/list">
+				<Button width="300px" colorScheme="teal" size='lg'>Restaurant List</Button>
+			</Link>
+		</div>
+	);
+};
 
 export default Home;
